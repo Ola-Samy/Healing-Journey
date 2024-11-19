06b5222260639b995da2c577b62d722fa89644d4
Ola-Samy <127288674+Ola-Samy@users.noreply.github.com>
2024-11-19 23:07:34 +0200

    Update About.js

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -30,7 +30,7 @@ function About() {
       </div>
       <div className="col-lg-5" style={{minHeight: 500}}>
         <div className="position-relative h-100">
-          <img className="position-absolute w-100 h-100 rounded wow zoomIn" data-wow-delay="0.9s" src="img/home4.jpg" style={{objectFit: 'cover'}} />
+          <img className="position-absolute w-100 h-100 rounded wow zoomIn" data-wow-delay="0.9s" src="../img/home4.jpg" style={{objectFit: 'cover'}} />
         </div>
       </div>
     </div>
